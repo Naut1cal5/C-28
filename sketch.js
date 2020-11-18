214cb0f93a4fec1e2fe3287ada43d1b53f80e400
sheema <sheemaparwaz@gmail.com>
2020-11-18 07:14:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-// namespacing(renaming)
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -6,7 +5,8 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg,platform;
+var bird, slingShot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,8 +17,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -34,18 +35,17 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-    //log6 = new Log (200,200,20, PI/2)
+
     bird = new Bird(100,100);
-    platform = new Ground (150,290,300,200)
-    slingshot = new Slingshot (bird.body,{x:200,y:100});
+
+   // log6 = new Log(230,180,80, PI/2);
+    slingShot = new Slingshot(bird.body,{x:200,y:100});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -60,15 +60,15 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-    //log6.display();
+
     bird.display();
     platform.display();
-    slingshot.display();
-    
+    //log6.display();
+    slingShot.display();    
 }
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY});
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
 }
-function mouseRelesed(){
-    slingshot.fly() 
+function mouseReleased(){
+    slingShot.fly();
 }
